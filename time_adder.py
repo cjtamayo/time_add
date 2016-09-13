72d79c5b64cabf1aa726595455feff0eb301427c
Christian Tamayo <ctamayo@newsinc.com>
2016-09-13 17:14:57 -0400

    adding time adder script

---
+++ time_adder.py
@@ -0,0 +1,48 @@
+__author__ = 'Christian Tamayo'
+
+timetime = []
+
+while True:
+    timething = raw_input('what is your time? Enter time, done, remove or reset\n')
+    if timething == 'done':
+        break
+    elif timething == 'reset':
+        timetime = []
+    elif timething == 'remove':
+        try:
+            timetime.pop()
+        except:
+            print 'Nothing to remove'
+            continue
+    else:
+        try:
+            split = timething.split(':')
+            assert type(int(split[0])) == int
+            assert type(int(split[1])) == int
+            timetime.append(timething)
+        except Exception as e:
+            print 'Please enter a valid time'
+            continue
+
+def time_add(time_list):
+    tot_mins = 0
+    tot_secs = 0
+    total = 0
+    for times in timetime:
+        split = times.split(':')
+        tot_mins += int(split[0])
+        tot_secs += int(split[1])
+    tot_mins += tot_secs/60
+    secs = str(tot_secs%60)
+    if len(secs) == 1:
+        secs = secs + '0'
+    if tot_mins >= 60:
+        new_tot_mins = tot_mins/60
+        new_new_tm = str(tot_mins - new_tot_mins*60)
+        if len(new_new_tm) == 1:
+            new_new_tm = '0'+ new_new_tm
+        return str(new_tot_mins) + ':' + new_new_tm + ':' + secs
+    else:
+        return str(tot_mins) + ':' + secs
+
+print time_add(timetime)
